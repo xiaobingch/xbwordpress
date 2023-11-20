adacb70b483f3a00bd59d2de4aea3ab65df0fba1
xiaobingch <xiaobingch@gmail.com>
2023-11-20 16:23:59 +0800

    自定义作者插件、日历事件插件

---
+++ wp-content/plugins/the-events-calendar/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'the-events-calendar/the-events-calendar',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2818b3aa2f60fd6242a4ec661614c636c325e1b1',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'the-events-calendar/the-events-calendar' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2818b3aa2f60fd6242a4ec661614c636c325e1b1',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'woocommerce/action-scheduler' => array(
+            'pretty_version' => '3.6.4',
+            'version' => '3.6.4.0',
+            'reference' => 'e7dafe215be5ef823392204535ee2e241f99a8cb',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../woocommerce/action-scheduler',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
